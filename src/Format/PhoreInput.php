d81940a3f132d7fe6dc91273e3df9ab28817633a
Jan <jan@talpa-solutions.com>
2019-04-08 15:48:13 +0200

    added TotimestampUTC Logic

--- src/Format/PhoreInput.php
+++ src/Format/PhoreInput.php
@@ -9,13 +9,33 @@
 namespace Phore\Core\Format;
 
 
+use http\Exception\InvalidArgumentException;
+
 class PhoreInput
 {
+    private $format = ["Y-m-d\TH:i:sP","Y-m-d\TH:i:s","Y-m-d\TH:i:s.uP","Y-m-d\TH:i:s.u","Y-m-d"];
 
-
-    function toTimestampUtc($input) : float
+    private function validateDate($date, $format)
     {
-
+        foreach ($format as $form){
+            $d = \DateTime::createFromFormat($form, $date);
+            if($d && $d->format($form) == $date){
+                return true;
+            }
+        }
+        return false;
     }
 
+    public function toTimestampUtc($input) : float
+    {
+        if(is_numeric($input) && strtotime(date('Y-m-d H:i:s',$input)) === (int)$input) {
+            return $input;
+        } else {
+            if(!$this->validateDate($input, $this->format)){
+                throw new InvalidArgumentException("Wrong Data Format");
+            }
+            $date = new \DateTime($input);
+            return $date->format('U.u');
+        }
+    }
 }
--- tests/unit/PhoreInputTest.php
+++ tests/unit/PhoreInputTest.php
@@ -9,10 +9,32 @@
 namespace Test;
 
 
+use Phore\Core\Format\PhoreInput;
 use PHPUnit\Framework\TestCase;
 
 class PhoreInputTest extends TestCase
 {
-
-
+    public function testToTimestampUtc(){
+        $phoreInput = new PhoreInput();
+        //Timestamp with mircoseconds
+        $this->assertEquals(1554717860.546789, $phoreInput->toTimestampUtc("1554717860.546789"));
+        //Timestamp
+        $this->assertEquals(1554717860, $phoreInput->toTimestampUtc("1554717860"));
+        //Format = Y-m-d\TH:i:s
+        $this->assertEquals(1554725060, $phoreInput->toTimestampUtc("2019-04-08T12:04:20"));
+        //Format = Y-m-d\TH:i:s.u
+        $this->assertEquals(1554725060.123456, $phoreInput->toTimestampUtc("2019-04-08T12:04:20.123456"));
+        //Format = Y-m-d\TH:i:sP
+        $this->assertEquals(1554717860, $phoreInput->toTimestampUtc("2019-04-08T12:04:20+02:00"));
+        //Format = Y-m-d\TH:i:s.uP
+        $this->assertEquals(1554717860.123456, $phoreInput->toTimestampUtc("2019-04-08T12:04:20.123456+02:00"));
+        //Format = Y-m-d
+        $this->assertEquals(1554681600.0, $phoreInput->toTimestampUtc("2019-04-08"));
+        //Exception
+        try{
+            $phoreInput->toTimestampUtc("05-2019-03");
+        } catch (\InvalidArgumentException $e){
+            $this->assertEquals($e->getMessage(), "Wrong Data Format");
+        }
+    }
 }
