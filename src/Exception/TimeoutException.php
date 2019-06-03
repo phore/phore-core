a46fc77691f10aa739208c5e7297052418388e76
Matthias <m@tth.es>
2019-06-03 11:03:08 +0200

    added timeout exception

---
+++ src/Exception/TimeoutException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthias
+ * Date: 03.06.19
+ * Time: 11:01
+ */
+
+namespace Phore\Core\Exception;
+
+
+class TimeoutException extends PhoreException
+{
+
+}
