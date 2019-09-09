4a28edaba955df7317aefa31fb618f8324e6fb8e
Michael <michael.schaar@talpa-solutions.com>
2019-09-09 15:14:38 +0200

    phore input toTimestampUTC removed conflicting ts validation

--- src/Format/PhoreInput.php
+++ src/Format/PhoreInput.php
@@ -26,7 +26,7 @@ class PhoreInput
 
     public function toTimestampUtc($input) : float
     {
-        if(is_numeric($input) && strtotime(date('Y-m-d H:i:s',$input)) === (int)$input) {
+        if(is_numeric($input)) {
             return $input;
         } else {
             if(!$this->validateDate($input, $this->format)){
